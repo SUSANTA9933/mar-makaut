e3139e6c0a68f47c4a8e3a34fceb4b30576a44f6
SUSANTA9933 <65112740+SUSANTA9933@users.noreply.github.com>
2020-05-10 15:46:59 +0530

    Add files via upload

---
+++ mar2020.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define MAXSIZE 10
+main()
+{
+	int array[MAXSIZE];
+	int i,j,num,temp;
+	printf("enter yhe value of num \n");
+	scanf("%d",&num);
+	printf("enter the element one  by one \n");
+	for (i=0; i<num; i++)
+	{
+		scanf("%d",&array[i]);
+		
+	}
+	for(i=0;i<num;i++)
+	{
+		for(j=0; j<(num-i-1);j++)
+		{
+			if (array[j]>array[j+1])
+			{
+				temp=array[j];
+				array[j]=array[j+1];
+				array[j+1]=temp;
+			}
+		}
+	}
+	printf("sorted array is...\n");
+	for(i=0;i<num;i++)
+	{
+		printf("%d\n",array[i]);
+	}
+}
